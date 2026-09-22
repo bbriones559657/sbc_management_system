8c776f122398880ee6f4382e38121f247e502485
bbriones559657 <b.briones.559657@umindanao.edu.ph>
2026-09-23 00:43:18 +0800

    Extend inventory repository for Supabase operations

--- lib/domain/repositories/inventory_repository.dart
+++ lib/domain/repositories/inventory_repository.dart
@@ -1,9 +1,40 @@
 import '../../models/inventory_item.dart';
+import '../../models/inventory_reference.dart';
 
 abstract class InventoryRepository {
   Future<List<InventoryItem>> getInventoryItems();
 
   Future<InventoryItem?> getInventoryItemById(String id);
+
+  Future<List<InventoryCategoryOption>> getCategories();
+
+  Future<List<InventoryUnitOption>> getUnits();
+
+  Future<List<InventoryMovementRecord>> getRecentMovements(
+    String inventoryItemId, {
+    int limit = 8,
+  });
+
+  Future<void> createInventoryItemWithInitialStock({
+    required String name,
+    required String categoryId,
+    required String baseUomId,
+    String sku = '',
+    bool trackExpiry = false,
+    double reorderLevel = 0,
+    double initialQuantity = 0,
+    DateTime? expirationDate,
+    double unitCostBase = 0,
+  });
+
+  Future<void> adjustStock({
+    required String inventoryItemId,
+    required String movementType,
+    required double quantity,
+    required String reason,
+    DateTime? expirationDate,
+    double unitCostBase = 0,
+  });
 
   Future<void> createInventoryItem(InventoryItem item);
 
