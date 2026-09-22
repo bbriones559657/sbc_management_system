1966f9e1ccdeb80da7aa85326c4499a0a33921f0
bbriones559657 <b.briones.559657@umindanao.edu.ph>
2026-09-22 23:32:28 +0800

    Add app navigation item model

---
+++ lib/models/app_navigation_item.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppNavigationItem {
+  final String label;
+  final IconData icon;
+
+  const AppNavigationItem({
+    required this.label,
+    required this.icon,
+  });
+}
